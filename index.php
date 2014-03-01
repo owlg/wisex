966542399cef9bbc489e5634d84679c62b1000ab
owlg <owlg@users.noreply.github.com>
2014-03-01 12:01:58 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,138 @@
-<!DOCTYPE HTML><html><head><title>WiseX - Welcome</title><meta http-equiv=content-type content="text/html; charset=utf-8" /><meta name=description content /><meta name=keywords content /><link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600" rel=stylesheet type=text/css /><!--[if lte IE 8]><script src=js/html5shiv.js></script><![endif]--><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script><script async="true" src="http://www.owlg.org/wisex/js/jquery.dropotron.min.js"></script><script src="http://www.owlg.org/wisex/js/skel.min.js"></script><script src="http://www.owlg.org/wisex/js/skel-panels.min.js"></script><script src="http://www.owlg.org/wisex/js/init.js"></script><noscript><link rel=stylesheet href="http://www.owlg.org/wisex/css/skel-noscript.css" /><link rel=stylesheet href="http://www.owlg.org/wisex/css/style.css" /><link rel=stylesheet href="http://www.owlg.org/wisex/css/style-desktop.css" /><link rel=stylesheet href="http://www.owlg.org/wisex/css/style-noscript.css" /></noscript><!--[if lte IE 8]><link rel=stylesheet href=css/ie8.css /><![endif]--></head><body><div id="fb-root"></div><script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=468230926614503"; fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script></body><body class=homepage><div id=header><div class=inner><header><h1><a href=# id=logo>WiseX</a></h1><hr /><span class=byline>Start playing and enjoying the game.</span></header><footer><a href=#banner class="button circled scrolly">Start</a></footer></div><nav id=nav><ul><li><a href=index.html>Home</a></li><li><span>Dropdown</span><ul><li><a href=#>x</a></li><li><a href=#>xx</a></li><li><a href=#>xxx</a></li><li><span>submenu &hellip;</span><ul><li><a href=#>x</a></li><li><a href=#>xx</a></li><li><a href=#>xxx</a></li><li><a href=#>xxxx</a></li></ul></li><li><a href=#>dzx</a></li></ul></li><li><a href=xxxx>Left Sidebar</a></li><li><a href=xxxxxxx>Right Sidebar</a></li><li><a href=xxxxxxxxx>No Sidebar</a></li></ul></nav></div><div id=banner><h2>Hi. Start playing <strong>WiseX</strong>.</h2><span class=byline>Best template built by <a href=http://owlg.org/>OWLG</a> with collaboration with <a href=http://www.fb.com/aqwps/>WiseX</a> and Wise Entertainment.</span></div><div class=carousel><div class=reel><article><a href=http://mdomaradzki.deviantart.com/art/Bueller-III-351975087 class="image featured"><img src=http://th01.deviantart.net/fs70/PRE/f/2013/033/8/5/bueller_iii_by_mdomaradzki-d5tk1pr.jpg alt /></a><header><h3><a href=#>Pulvinar sagittis congue</a></h3></header><p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p></article><article><a href=http://mdomaradzki.deviantart.com/art/Disco-351602759 class="image featured"><img src=http://fc01.deviantart.net/fs70/f/2013/031/3/6/disco_by_mdomaradzki-d5tc2fb.jpg alt /></a><header><h3><a href=#>Fermentum sagittis proin</a></h3></header><p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p></article></div></div><div class="wrapper style2"><article id=main class="container special"><center><object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" id="u35941" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,28,0" width="960" height="550"> <param name="movie" value="gamefiles/GRAVEYARD.swf" /> <param name="LOOP" value="false" /> <param name="SCALE" value="exactfit" /> <param name="allowScriptAccess" value="always" /> <param name="allowFullScreen" value="true" /> <param name="menu" value="false" /> <param name="wmode" value="window" /> <param name="FlashVars" value="&sLang=en" /> <embed src="/gamefiles/loader_r8.swf" width="940" height="550" loop="false" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" scale="exactfit" allowscriptaccess="always" allowFullScreen="true" menu="false" flashvars="&sLang=en"></embed> </object></center><center><iframe src="https://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Faqwps&amp;width=600&amp;height=427&amp;colorscheme=light&amp;show_faces=false&amp;header=true&amp;stream=true&amp;show_border=true&amp;appId=468230926614503" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:600px; height:427px;" allowTransparency="true"></iframe></center><br/><header><ul class=icons><li><a href=http://www.twitter.com/owlg/ class="fa fa-twitter solo"><span>Twitter</span></a></li><li><a href=http://www.fb.com/aqwps/ class="fa fa-facebook solo"><span>Facebook</span></a></li><li><a href=# class="fa fa-google-plus solo"><span>Google+</span></a></li><li><a href=# class="fa fa-pinterest solo"><span>Pinterest</span></a></li><li><a href="http://www.owlg.org/blog"class="fa fa-dribbble solo"><span>Website</span></a></li><li><a href=# class="fa fa-linkedin solo"><span>Linkedin</span></a></li></ul></section><div class=copyright><ul class=menu><li>&copy; WiseX. All rights reserved.</li><iframe async="true" width="600" src=http://www.owlg.org/blog/></iframe><li>Design: <a href=http://owlg.org/>OWLG</a></li></ul></div></div></div></div></div></body></html>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>WiseX - Welcome</title>
+<meta http-equiv=content-type content="text/html; charset=utf-8" />
+<meta name=description content />
+<meta name=keywords content />
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600" rel=stylesheet type=text/css />
+<!--[if lte IE 8]><script src=js/html5shiv.js></script><![endif]-->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script async="true" src="http://www.owlg.org/wisex/js/jquery.dropotron.min.js"></script>
+<script src="http://www.owlg.org/wisex/js/skel.min.js"></script>
+<script src="http://www.owlg.org/wisex/js/skel-panels.min.js"></script>
+<script src="http://www.owlg.org/wisex/js/init.js"></script>
+<noscript>
+<link rel=stylesheet href="http://www.owlg.org/wisex/css/skel-noscript.css" />
+<link rel=stylesheet href="http://www.owlg.org/wisex/css/style.css" />
+<link rel=stylesheet href="http://www.owlg.org/wisex/css/style-desktop.css" />
+<link rel=stylesheet href="http://www.owlg.org/wisex/css/style-noscript.css" />
+</noscript>
+<!--[if lte IE 8]><link rel=stylesheet href=css/ie8.css /><![endif]-->
+</head>
+<body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=468230926614503";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+</body>
+<body class=homepage>
+<div id=header>
+<div class=inner>
+<header>
+<h1><a href=# id=logo>WiseX</a></h1>
+<hr />
+<span class=byline>Start playing and enjoying the game.</span>
+</header>
+<footer>
+<a href=#banner class="button circled scrolly">Start</a>
+</footer>
+</div>
+<nav id=nav>
+<ul>
+<li><a href=index.html>Home</a></li>
+<li>
+<span>Dropdown</span>
+<ul>
+<li><a href=#>x</a></li>
+<li><a href=#>xx</a></li>
+<li><a href=#>xxx</a></li>
+<li>
+<span>submenu &hellip;</span>
+<ul>
+<li><a href=#>x</a></li>
+<li><a href=#>xx</a></li>
+<li><a href=#>xxx</a></li>
+<li><a href=#>xxxx</a></li>
+</ul>
+</li>
+<li><a href=#>dzx</a></li>
+</ul>
+</li>
+<li><a href=xxxx>Left Sidebar</a></li>
+<li><a href=xxxxxxx>Right Sidebar</a></li>
+<li><a href=xxxxxxxxx>No Sidebar</a></li>
+</ul>
+</nav>
+</div>
+<div id=banner>
+<h2>Hi. Start playing <strong>WiseX</strong>.</h2>
+<span class=byline>
+Best template built by <a href=http://owlg.org/>OWLG</a> with collaboration with <a href=http://www.fb.com/aqwps/>WiseX</a> and Wise Entertainment.
+</span>
+</div>
+<div class=carousel>
+<div class=reel>
+<article>
+<a href=http://mdomaradzki.deviantart.com/art/Bueller-III-351975087 class="image featured"><img src=http://th01.deviantart.net/fs70/PRE/f/2013/033/8/5/bueller_iii_by_mdomaradzki-d5tk1pr.jpg alt /></a>
+<header>
+<h3><a href=#>Pulvinar sagittis congue</a></h3>
+</header>
+<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+</article>
+<article>
+<a href=http://mdomaradzki.deviantart.com/art/Disco-351602759 class="image featured"><img src=http://fc01.deviantart.net/fs70/f/2013/031/3/6/disco_by_mdomaradzki-d5tc2fb.jpg alt /></a>
+<header>
+<h3><a href=#>Fermentum sagittis proin</a></h3>
+</header>
+<p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+</article>
+</div>
+</div>
+<div class="wrapper style2">
+<article id=main class="container special">
+<center>
+
+<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" id="u35941" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,28,0" width="960" height="550">
+	          <param name="movie" value="gamefiles/GRAVEYARD.swf" />
+	          <param name="LOOP" value="false" />
+	          <param name="SCALE" value="exactfit" />
+	          <param name="allowScriptAccess" value="always" />
+			  <param name="allowFullScreen" value="true" />
+	          <param name="menu" value="false" />
+	          <param name="wmode" value="window" />
+	          <param name="FlashVars" value="&sLang=en" />
+	          <embed src="/gamefiles/loader_r8.swf" width="940" height="550" loop="false" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" scale="exactfit" allowscriptaccess="always" allowFullScreen="true" menu="false" flashvars="&sLang=en"></embed>
+	        </object>
+			</center>
+<center>
+<iframe src="https://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Faqwps&amp;width=600&amp;height=427&amp;colorscheme=light&amp;show_faces=false&amp;header=true&amp;stream=true&amp;show_border=true&amp;appId=468230926614503" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:600px; height:427px;" allowTransparency="true"></iframe>
+</center>
+<br/>
+<header>
+<ul class=icons>
+<li><a href=http://www.twitter.com/owlg/ class="fa fa-twitter solo"><span>Twitter</span></a></li>
+<li><a href=http://www.fb.com/aqwps/ class="fa fa-facebook solo"><span>Facebook</span></a></li>
+<li><a href=# class="fa fa-google-plus solo"><span>Google+</span></a></li>
+<li><a href=# class="fa fa-pinterest solo"><span>Pinterest</span></a></li>
+<li><a href="http://www.owlg.org/blog"class="fa fa-dribbble solo"><span>Website</span></a></li>
+<li><a href=# class="fa fa-linkedin solo"><span>Linkedin</span></a></li>
+</ul>
+</section>
+<div class=copyright>
+<ul class=menu>
+<li>&copy; WiseX. All rights reserved.</li>
+<iframe async="true" width="600" src=http://www.owlg.org/blog/></iframe>
+<li>Design: <a href=http://owlg.org/>OWLG</a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
